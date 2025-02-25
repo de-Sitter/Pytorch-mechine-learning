c3a12442751459438275527492319d308908623e
de-Sitter <haozheshi869@gmail.com>
2025-02-25 10:40:41 +0800

    Add files via upload

---
+++ pythorch.py
@@ -0,0 +1,100 @@
+import torch
+import torch.nn as nn  
+import torch.optim as optim  
+import torchvision  
+import torchvision.transforms as transforms  
+from torch.utils.data import DataLoader  
+import matplotlib.pyplot as plt  
+import numpy as np
+
+transform = transforms.Compose([
+    transforms.ToTensor(), 
+    transforms.Normalize((0.5,), (0.5,)) 
+])
+
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+batch_size = 100  
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(32 * 4 * 4, 120) 
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10) 
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(-1, 32 * 4 * 4) 
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x) 
+        return x
+
+device = torch.device("cpu")
+model = CNN().to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 5  
+for epoch in range(num_epochs):
+    model.train() 
+    running_loss = 0.0
+    for i, (images, labels) in enumerate(train_loader):
+
+        images, labels = images.to(device), labels.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()  
+        loss.backward()  
+        optimizer.step()  
+
+        running_loss += loss.item()
+        if (i + 1) % 100 == 0:  
+            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader)}], Loss: {running_loss/100:.4f}')
+            running_loss = 0.0
+
+print('Training finished!')
+
+model.eval() 
+correct = 0
+total = 0
+
+with torch.no_grad(): 
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1) 
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+accuracy = 100 * correct / total
+print(f'Test Accuracy: {accuracy:.2f}%')
+
+def visualize_predictions(model, test_loader, num_images=5):
+    model.eval()
+    images, labels = next(iter(test_loader))
+    images, labels = images.to(device), labels.to(device)
+    outputs = model(images)
+    _, predicted = torch.max(outputs, 1)
+
+    plt.figure(figsize=(10, 2))
+    for i in range(num_images):
+        plt.subplot(1, num_images, i+1)
+        plt.imshow(images[i][0].cpu().numpy(), cmap='gray')
+        plt.title(f'Pred: {predicted[i].item()}\nTrue: {labels[i].item()}')
+        plt.axis('off')
+    plt.show()
+
+visualize_predictions(model, test_loader)
